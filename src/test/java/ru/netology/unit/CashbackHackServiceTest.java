007f619fb91d93f586df5e53cde7c7e3a01f678d
Екатерина Кокарева <kokarevakatja@gmail.com>
2022-09-26 18:05:23 +0300

    Junit4 added

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.unit;
 
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
